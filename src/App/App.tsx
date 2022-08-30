95f25e36f143a7feb0cce065684332cc1ce1731c
Mikhail <mikhail@MacBook-Air.local>
2022-08-30 23:22:13 +0300

    Before changes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 
-import Header from "@components/Header";
 import { BrowserRouter } from "react-router-dom";
-
-import Content from "../Content";
 import "@styles/style.scss";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="container">
-        <Header />
-        <Content />
-      </div>
+      <div className="container"></div>
     </BrowserRouter>
   );
 };
---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "@components/Header";
+import Content from "src/Content";
+
+const Layout = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Content />
+    </div>
+  );
+};
+
+export default Layout;
